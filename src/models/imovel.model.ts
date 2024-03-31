650605f18298ac469918980b9ae2cf5a16461a3d
Luigi <luigilucas95@gmail.com>
2024-03-31 12:40:56 -0300

    Adicionando o mapeamento das entidades e seus relacionamentos

---
+++ src/models/imovel.model.ts
@@ -0,0 +1,113 @@
+import { Sequelize, DataTypes, Model } from 'sequelize';
+import { Application } from '../declarations';
+import { HookReturn } from 'sequelize/types/hooks';
+
+export default function (app: Application): typeof Model {
+  const sequelizeClient: Sequelize = app.get('sequelizeClient');
+  const imovel = sequelizeClient.define('imovel', {
+
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+
+    proprietario_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      unique: true,
+      references: {
+        model: 'usuario',
+        key: 'id'
+      }
+    },
+
+    anuncio_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: 'anuncio',
+        key: 'id'
+      }
+    },
+
+    nome: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    endereco: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    tipo: {
+      type: DataTypes.STRING
+    },
+
+    imagens: {
+      type: DataTypes.BLOB,
+      allowNull: false
+    },
+
+    videos: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
+    qtdQuartos: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+
+    maxPessoas: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+
+    precoPadrao: {
+      type: DataTypes.DECIMAL,
+      allowNull: false
+    },
+
+    descricao: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    }
+
+
+  }, {
+    hooks: {
+      beforeCount(options: any): HookReturn {
+        options.raw = true;
+      }
+    }
+  });
+
+  (imovel as any).associate = function (models: any): void {
+    imovel.belongsTo(models.usuario, {
+      foreignKey: 'proprietario_id',
+      constraints: true
+    })
+
+    imovel.hasMany(models.locacao, {
+      foreignKey: 'imovel_id'
+    })
+
+    imovel.hasMany(models.pacote, {
+      foreignKey: 'imovel_id'
+    })
+
+    imovel.hasOne(models.anuncio, {
+      foreignKey: 'anuncio_id',
+      constraints: true
+    })
+
+    imovel.hasMany(models.duvida, {
+      foreignKey: 'imovel_id'
+    })
+  };
+
+  return imovel;
+}
--- src/models/usuario.model.ts
+++ src/models/usuario.model.ts
@@ -1,5 +1,3 @@
-// See https://sequelize.org/master/manual/model-basics.html
-// for more of what you can do here.
 import { Sequelize, DataTypes, Model } from 'sequelize';
 import { Application } from '../declarations';
 import { HookReturn } from 'sequelize/types/hooks';
@@ -7,18 +5,52 @@ import { HookReturn } from 'sequelize/types/hooks';
 export default function (app: Application): typeof Model {
   const sequelizeClient: Sequelize = app.get('sequelizeClient');
   const usuario = sequelizeClient.define('usuario', {
-  
+
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+
+    nome: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    cpf: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+
+    rg: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+
+    telefone: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    endereco: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
     email: {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true
     },
+
     password: {
       type: DataTypes.STRING,
       allowNull: false
     },
-  
-  
+
   }, {
     hooks: {
       beforeCount(options: any): HookReturn {
@@ -27,10 +59,14 @@ export default function (app: Application): typeof Model {
     }
   });
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   (usuario as any).associate = function (models: any): void {
-    // Define associations here
-    // See https://sequelize.org/master/manual/assocs.html
+    usuario.hasMany(models.imovel, {
+      foreignKey: 'proprietario_id'
+    })
+
+    usuario.hasMany(models.locacao, {
+      foreignKey: 'locador_id'
+    })
   };
 
   return usuario;
